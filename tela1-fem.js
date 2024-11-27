d0b654a9c100ae14c9e46965da4e558d9dd5af27
EdsonFreitas0 <142626253+EdsonFreitas0@users.noreply.github.com>
2024-11-27 09:54:39 -0300

    update

---
+++ tela1-fem.js
@@ -0,0 +1,3 @@
+function escolher(opcao) {
+    localStorage.setItem("escolha1", opcao); // Armazena a escolha
+    window.location.href = "tela2-fem.html"; }   // Vai para a próxima tela
--- telamasefem.js
+++ telamasefem.js
@@ -1,3 +1,6 @@
-function escolher(opcao) {
-    localStorage.setItem("escolha1", opcao); // Armazena a escolha
-    window.location.href = "tela-1.html"; }   // Vai para a próxima tela
+function escolhermas(opcao_mas) {
+    localStorage.setItem("escolha1", opcao_mas); // Armazena a escolha
+    window.location.href = "tela1-mas.html"; }   // Vai para a próxima tela
+function escolherfem(opcao_fem) {
+    localStorage.setItem("escolha1", opcao_fem); // Armazena a escolha
+    window.location.href = "tela1-fem.html"; }   // Vai para a próxima tela
